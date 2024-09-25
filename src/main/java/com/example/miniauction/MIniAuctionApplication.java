fbbd478ab387de36e81ba8f3c077ae4f3792b1ec
CJ-Park <king01286@naver.com>
2024-09-25 15:53:48 +0900

    First Commit

---
+++ src/main/java/com/example/miniauction/MIniAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.example.miniauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MIniAuctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MIniAuctionApplication.class, args);
+    }
+
+}
