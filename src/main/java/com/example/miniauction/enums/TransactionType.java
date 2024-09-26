7f3362ad0164c3387300c057429db4de068d57c7
CJ-Park <king01286@naver.com>
2024-09-26 15:26:42 +0900

    TransactionType 네이밍 수정

--- src/main/java/com/example/miniauction/enums/TransactionType.java
+++ src/main/java/com/example/miniauction/enums/TransactionType.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum TransactionType {
     DEPOSIT("입금"), WITHDRAWAL("출금"),
-    BIDDING("입찰"), BID_RETURN("입찰금 반환");
+    BIDDING("입찰"), REFUND("입찰금 반환");
 
     private final String description;
 }
